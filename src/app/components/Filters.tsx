e937405455aaad7e5131344ab6db5ce7d87b34f2
Koen van den Berg <git@koenvandenberg.dev>
2023-07-07 10:55:01 +0200

    Adjust stacking behaviour of filters on mobile

--- src/app/components/Filters.tsx
+++ src/app/components/Filters.tsx
@@ -4,7 +4,7 @@ import RegionFilter from "./RegionFilter";
 
 export default function Filters() {
   return (
-    <div className="w-full flex flex-col sm:flex-row justify-between">
+    <div className="w-full flex flex-col sm:flex-row justify-between gap-4">
       <SearchFilter />
       <RegionFilter />
     </div>
