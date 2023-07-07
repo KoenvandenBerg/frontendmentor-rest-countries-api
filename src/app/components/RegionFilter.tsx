4d9af431f9c72b24acecab052d90df789e9737f8
Koen van den Berg <git@koenvandenberg.dev>
2023-07-07 11:10:04 +0200

    Add dropdown menu for the region filter

--- src/app/components/RegionFilter.tsx
+++ src/app/components/RegionFilter.tsx
@@ -12,22 +12,38 @@ export default function RegionFilter() {
 
   const [selectedRegion, setSelectedRegion] = useState("");
 
+  const selectRegion = (region: string) => {
+    setSelectedRegion(region);
+    setMenuOpen(false);
+  };
+
   return (
-    <div className="relative h-12 sm:h-[3.5rem] w-full sm:w-[12.5rem] flex justify-between items-center bg-light-elements dark:bg-dark-elements shadow-md shadow-box-shadow rounded-[0.3125rem] px-6 sm:px-6 text-xs sm:text-sm text-light-text dark:text-dark-input">
-      <p>{selectedRegion !== "" ? selectedRegion : "Filter by Region"}</p>
-      {selectedRegion !== "" ? (
-        <MdClose
-          onClick={() => setSelectedRegion("")}
-          className="absolute h-4 sm:h-5 w-4 sm:w-5 text-light-input dark:text-dark-input right-4 top-4 sm:top-[1.15rem] hover:text-red-700 dark:hover:text-red-700 hover:scale-150 cursor-pointer transition-transform"
-        />
-      ) : (
-        <button onClick={toggleMenu}>
-          <MdArrowDropDown
-            className={`h-6 w-6 mr-[-0.3rem] ${
-              menuOpen && "rotate-180"
-            } transition-all hover:scale-125`}
+    <div>
+      <div className="relative h-12 sm:h-[3.5rem] w-full sm:w-[12.5rem] flex justify-between items-center bg-light-elements dark:bg-dark-elements shadow-md shadow-box-shadow rounded-[0.3125rem] px-6 sm:px-6 text-xs sm:text-sm text-light-text dark:text-dark-input">
+        <p>{selectedRegion !== "" ? selectedRegion : "Filter by Region"}</p>
+        {selectedRegion !== "" ? (
+          <MdClose
+            onClick={() => setSelectedRegion("")}
+            className="absolute h-4 sm:h-5 w-4 sm:w-5 text-light-input dark:text-dark-input right-4 top-4 sm:top-[1.15rem] hover:text-red-700 dark:hover:text-red-700 hover:scale-150 cursor-pointer transition-transform"
           />
-        </button>
+        ) : (
+          <button onClick={toggleMenu}>
+            <MdArrowDropDown
+              className={`h-6 w-6 mr-[-0.3rem] ${
+                menuOpen && "rotate-180"
+              } transition-all hover:scale-125`}
+            />
+          </button>
+        )}
+      </div>
+      {menuOpen && (
+        <div className="relative top-1 w-full sm:w-[12.5rem] py-4 bg-light-elements dark:bg-dark-elements shadow-md shadow-box-shadow rounded-[0.3125rem] px-6 sm:px-6 text-xs sm:text-sm text-light-text dark:text-dark-input">
+          <ul className="flex flex-col gap-2">
+            <li onClick={() => selectRegion("Test 1")}>Menu Item 1</li>
+            <li>Menu Item 2</li>
+            <li>Menu Item 3</li>
+          </ul>
+        </div>
       )}
     </div>
   );
