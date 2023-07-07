7731cfc1e8d5e838b980d1c899149833d8cc8b54
Koen van den Berg <git@koenvandenberg.dev>
2023-07-07 18:49:40 +0200

    Truncate long country names

--- src/app/components/CountryCard.tsx
+++ src/app/components/CountryCard.tsx
@@ -19,8 +19,8 @@ export default function CountryCard(props: countryCardProps) {
           alt={`The flag of ${props.name}.`}
           className="h-1/2 w-full rounded-t-[0.3125rem]"
         />
-        <div className="flex flex-col gap-1 pt-6 pl-6">
-          <h2 className="text-lg font-extrabold pb-2">{props.name}</h2>
+        <div className="flex flex-col gap-1 m-6">
+          <h2 className="text-lg font-extrabold pb-2 truncate">{props.name}</h2>
           <p className="text-sm">
             <span className="font-bold">Population:</span> {props.population}
           </p>
