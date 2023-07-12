e70f356253b3f6387e9be49c73e82737df99523a
Koen van den Berg <git@koenvandenberg.dev>
2023-07-12 16:33:58 +0200

    Add an error page

---
+++ src/app/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import BackButton from "./components/BackButton";
+import { MdOutlineErrorOutline } from "react-icons/md";
+
+export default function error() {
+  return (
+    <div className="h-[25vh] flex flex-col justify-center items-center">
+      <MdOutlineErrorOutline className="w-10 h-10 mb-2" />
+      <h2 className="text-2xl mb-12">An error occurred.</h2>
+      <BackButton />
+    </div>
+  );
+}
