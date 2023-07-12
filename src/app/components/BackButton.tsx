9f41f4a9c53d2ced68f52e423e3cc44084bb97b9
Koen van den Berg <git@koenvandenberg.dev>
2023-07-12 15:59:25 +0200

    Adjust styling back button

--- src/app/components/BackButton.tsx
+++ src/app/components/BackButton.tsx
@@ -9,9 +9,9 @@ export default function BackButton() {
   return (
     <button
       onClick={() => router.back()}
-      className="flex justify-between items-center px-10 w-[6.5rem] h-8 sm:w-[7.5rem] sm:h-9 md:w-[8.5rem] md:h-10 bg-light-elements dark:bg-dark-elements rounded-[0.3125rem] shadow-md shadow-box-shadow hover:scale-105"
+      className="flex justify-between items-center px-6 sm:px-8 w-[6.5rem] h-8 sm:w-[7.5rem] sm:h-9 md:w-[8.5rem] md:h-10 bg-light-elements dark:bg-dark-elements rounded-[0.3125rem] shadow-md shadow-box-shadow hover:scale-105 text-sm md:text-base"
     >
-      <MdArrowBack className="w-4 h-4" />
+      <MdArrowBack className="h-4 md:h-6 w-4 md:w-6" />
       Back
     </button>
   );
